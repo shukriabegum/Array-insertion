102768aac3aff2c75b3d811c31a13bce9ad8cf83
Shukria Begum <shukriabegum2004@gmail.com>
2025-09-26 18:14:41 +0600

    Add files via upload
    add multiple insertion file

---
+++ Multiple insertion in a array.cpp
@@ -0,0 +1,51 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,n;
+    cout<<"Enter a size:";
+    cin>>n;
+    int a[n+1];
+    for(i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    int beginningValue;
+    cout<<"Enter a beginning value:";
+    cin>>beginningValue;
+    for(i=n-1; i>=0; i--)
+    {
+        a[i+1]=a[i];
+    }
+    a[0]=beginningValue;
+    n++;
+
+    int pos;
+    cout<<"Enter position:";
+    cin>>pos;
+    while(pos<1||pos>n+1)
+    {
+        cout<<"Invalid";
+        cout<<"Enter position:";
+        cin>>pos;
+    }
+    int value;
+    cout<<"Enter a value :";
+    cin>>value;
+    for(i=n-1; i>=pos-1; i--)
+    {
+        a[i+1]=a[i];
+    }
+    a[pos-1]=value;
+    n++;
+    int endingValue;
+    cout<<"Enter a ending value:";
+    cin>>endingValue;
+    a[n]=endingValue;
+
+    for(i=0; i<n+1; i++)
+    {
+        cout<<a[i]<<" ";
+    }
+
+}
